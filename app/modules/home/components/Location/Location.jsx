ada2bc479e866be6bfd0e892ec14e9ccd6662789
Prathamesh <pratham2604@gmail.com>
2019-06-08 16:19:46 +0530

    Add Location Box

--- app/modules/home/components/Location/Location.jsx
+++ app/modules/home/components/Location/Location.jsx
@@ -6,13 +6,40 @@ class Location extends Component {
         super(props);
         this.state = {
         };
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(event) {
+        const { value } = event.target;
+        this.props.selectLocations(value);
     }
 
     render() {
-        const { data } = this.props;
+        const { data, selectLocations, selectedLocations, searchString } = this.props;
+        const noResult = searchString && selectedLocations.length === 0;
+        const displayData = selectedLocations.length > 0 ? selectedLocations : data;
+
         return (
             <div className="location-dashboard">
-                Location
+                <div className="-search-container">
+                    <input className="-search-bar" name="locator" onChange={this.onChange} value={searchString}/>
+                </div>
+                {noResult ?
+                    <div className="-result">
+                        No Results found
+                    </div> :
+                    <div className="-result">
+                        {displayData.map((locationNode) => {
+                            const { id, pincode, location } = locationNode;
+                            return (
+                                <div key={id} className="-location">
+                                    <div className="-city">{location}</div>
+                                    <div className="-pincode">{pincode}</div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
             </div>
         );
     }
--- app/modules/home/components/index.jsx
+++ app/modules/home/components/index.jsx
@@ -7,17 +7,36 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            selectedLocations: [],
+            searchString: '',
         };
+        this.selectLocations = this.selectLocations.bind(this);
+    }
+
+    selectLocations(search) {
+        const selectedLocations = this.props.data.filter((location) => {
+            return location.pincode.toLowerCase().includes(search.toLowerCase());
+        })
+        this.setState({
+            searchString: search,
+            selectedLocations,
+        });
     }
 
     render() {
         const { data } = this.props;
-        console.log(data)
+        const { selectedLocations, searchString } = this.state;
+
         return (
-            <div className="home-dashboard container">
+            <div className="home-dashboard">
                 <div className="row">
                     <div className="col-sm-6">
-                        <Location />
+                        <Location
+                            data={data}
+                            selectLocations={this.selectLocations}
+                            selectedLocations={selectedLocations}
+                            searchString={searchString}
+                        />
                     </div>
                     <div className="col-sm-6">
                         <WorldMap />
